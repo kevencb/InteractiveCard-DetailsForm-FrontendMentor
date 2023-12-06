c64531d8177f67a6cdde1aca2da98ae4b40efebb
kevencb <kevcb369@gmail.com>
2023-12-05 22:48:43 -0500

    Validando ingreso de datos numéricos de la tarjeta

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ let nameCard = document.querySelector('.card-front__details-name');
 let input_nameCard = document.querySelector('#cardHolder');
 let error_nameCard = document.querySelector('.form__error--cardholder');
 
+//Datos del número de tarjeta
+let numberCard = document.querySelector('.card-front__number');
+let input_numberCard = document.querySelector('#cardNumber');
+let error_numberCard = document.querySelector('.form__error--cardnumber');
+
+
 input_nameCard.addEventListener('input', () => {
     if(input_nameCard.value == ''){
         nameCard.innerText = 'Jane Appleseed'
@@ -11,15 +17,16 @@ input_nameCard.addEventListener('input', () => {
     }
 })
 
-//Datos del Número de tarjeta
-let numberCard = document.querySelector('.card-front__number');
-let input_numberCard = document.querySelector('#cardNumber');
-let error_numberCars = document.querySelector('form__error--cardnumber');
+input_numberCard.addEventListener('input', ()=> {
 
-input_numberCard.addEventListener('input', () => {
+    numberCard.innerText = input_numberCard.value
+
+    let regExp = /[A-z]/g;
+    if(regExp.test(input_numberCard.value)){
+        error_numberCard.innerText = 'No!'
+    }
+
     if(input_numberCard.value == ''){
         numberCard.innerText = '0000 0000 0000 0000'
-    }else{
-        numberCard.innerText = input_numberCard.value
     }
 })
